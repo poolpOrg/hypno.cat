fa95da808d928ec66ab596922bd234356e136b41
agka <benoit.kogut@gmail.com>
2020-10-30 22:48:39 +0100

    trigger gallery on space key

--- src/scripts/photo-gallery.js
+++ src/scripts/photo-gallery.js
@@ -5,4 +5,18 @@ addEventHandler(window, "load", function () { // async load
     baguetteBox.run(".gallery", {
         buttons: true
     });
+
+    querySelectAll(".gallery-entry a")
+        .forEach(function ($a) {
+            addEventHandler($a, "keydown", function (e) {
+                if (e.code === "Space") {
+                    e.preventDefault();  // prevent page down scroll
+                    // trigger gallery after a small delay, as baguetteBox
+                    // consumes the 'space' key to switch to another image
+                    setTimeout(function () {
+                        $a.click();
+                    }, 200);
+                }
+            }, true);
+        });
 });
